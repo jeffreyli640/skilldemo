e5bdbd26798c288d0f3b25fd1f4cffd036878993
JeffreyLi <47624796+jeffreyli640@users.noreply.github.com>
2022-05-02 00:00:51 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testSubtract() {
-        assertEquals(4, SkillDemo.subtract(5, 3));
+        assertEquals(2, SkillDemo.subtract(5, 3));
     }
 }
